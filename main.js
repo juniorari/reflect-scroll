83fc750a4b143a81190974a30c39f6a4aa23b83a
ElderCarvalho <eld.carv@gmail.com>
2017-09-22 17:21:25 -0400

    refatoração para plugin

--- main.js
+++ main.js
@@ -1,30 +1,55 @@
-var phoneTrack       = document.querySelector('.phone-carousel');
-var phoneSlideHeight = document.querySelector('.phone-item').clientHeight;
-var sections         = document.querySelectorAll('.section');
-var sectionsNumber   = sections.length;
-var sectionHeight    = window.innerHeight;
-var stepPhone        = 0;
-var stepSection      = 0;
-var scrollHeight     = sectionHeight;
-// console.log(document.querySelector('.sections'));
+var rscroll = (function() {
+    var phoneTrack;
+    var phoneSlideHeight;
+    var sections;
+    var sectionHeight;
+    var sectionsNumber;
+    var stepPhone;
+    var stepSection;
+    var scrollHeight;
+    var pageHeight;
 
-Array.prototype.forEach.call(sections, function(el){
-  el.style.height =  sectionHeight + 'px';
-});
+    var $ = function(selector) {
+        if (selector.substring(0, 1) == '#') {
+            return document.getElementById(selector.substring(1, selector.length));
+        }
+        return document.querySelectorAll(selector);
+    }
 
-var pageHeight = document.body.scrollHeight;
-console.log(phoneTrack.clientHeight);
+    var initVariables = function(container, mirror) {
+        if (!$('.phone-carousel')[0]) {
+            console.log('Error: Nenhum elemento encontrado para o container');
+            return;
+        }
 
-console.log(phoneTrack);
-window.addEventListener('scroll', function(){
-// console.log(window.pageYOffset);
-  var percent = 100 * window.pageYOffset / (pageHeight - scrollHeight);
-  // console.log(percent);
+        sections = $(container)[0].querySelectorAll('.section');
+        sectionsNumber = sections.length;
+        sectionHeight = window.innerHeight;
+        scrollHeight = sectionHeight;
 
-  //stepPhone = ((pageHeight - phoneSlideHeight) / 100) * percent;
-  stepPhone = ((phoneTrack.clientHeight - phoneSlideHeight ) * percent) / 100;
-  // console.log("Y: "+phoneTrack.style.bot)
+        phoneTrack = $(mirror)[0];
+        phoneSlideHeight = $(mirror)[0].querySelector('.phone-item').clientHeight;
+        stepPhone = 0;
 
-  phoneTrack.style.transform = 'translateY(-'+stepPhone+'px)';
+        Array.prototype.forEach.call(sections, function(el) {
+            el.style.height = sectionHeight + 'px';
+        });
 
-});
+        pageHeight = document.body.scrollHeight;
+    }
+
+    return {
+        init: function(container, mirror) {
+            initVariables(container, mirror);
+
+            window.addEventListener('scroll', function() {
+                var percent = 100 * window.pageYOffset / (pageHeight - scrollHeight);
+
+                stepPhone = ((phoneTrack.clientHeight - phoneSlideHeight) * percent) / 100;
+
+                phoneTrack.style.transform = 'translateY(-' + stepPhone + 'px)';
+
+            });
+        }
+    }
+})();
